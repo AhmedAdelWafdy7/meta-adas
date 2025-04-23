cdb26a899b35cd38c827cf745840ac3c50795029
AhmedAdelWafdy7 <fandes99@gmail.com>
2025-04-23 18:38:45 +0200

    fix: update rfkill wifi issue

--- conf.sh
+++ conf.sh
@@ -26,50 +26,6 @@ echo -e "KERNEL_DEVICETREE:append = \" \\
     overlays/mcp251xfd.dtbo \\
 \"\n" >> $CONF
 
-echo "#Enable Wayland" >> $CONF
-echo -e "DISTRO_FEATURES:append = \"wayland\"" >> $CONF
-echo -e "CORE_IMAGE_EXTRA_INSTALL = \"wayland\"" >> $CONF
-echo -e "PACKAGECONFIG:remove:pn-qtwayland = \"xcomposite-glx\"" >> $CONF
+echo "LICENSE_FLAGS_ACCEPTED += \"synaptics-killswitch commercial\"" >> $CONF
+echo "IMAGE_INSTALL:append = \" wpa-supplicant iw wireless-regdb rfkill linux-firmware-bcm43430\"" >> $CONF
 
-echo "#Enable Camera" >> $CONF
-echo -e "VIDEO_CAMERA = \"1\"" >> $CONF
-
-echo "# IP Compliance about WiFi/BT pacakge" >> $CONF
-echo -e "LICENSE_FLAGS_ACCEPTED += \"synaptics-killswitch\"\n" >> $CONF
-
-# Add WiFi support
-echo "# Enable WiFi support" >> $CONF
-echo "DISTRO_FEATURES:append = \" wifi\"" >> $CONF
-echo "MACHINE_FEATURES:append = \" wifi bluetooth\"" >> $CONF
-echo "CORE_IMAGE_EXTRA_INSTALL:append = \" linux-firmware-rpidistro-bcm43455 wireless-regdb wpa-supplicant iw\"" >> $CONF
-
-# Define environment variables
-export NAMESPACE="sdv-system"
-export OTA_VERSION="1.0.0"
-export ECU_CORE_VERSION="1.0.0"
-
-# Create the namespace if it doesn't exist
-kubectl create namespace $NAMESPACE --dry-run=client -o yaml | kubectl apply -f -
-
-# Apply RBAC configurations
-kubectl apply -f Secure-OTA-Application/kubernetes/config/rbac.yaml -n $NAMESPACE
-
-# Apply ConfigMaps
-kubectl apply -f Secure-OTA-Application/kubernetes/config/ota-configmap.yaml -n $NAMESPACE
-
-# Create persistent volume claims
-kubectl apply -f Secure-OTA-Application/kubernetes/storage/storage-claims.yaml -n $NAMESPACE
-
-# Deploy OTA components
-kubectl apply -f Secure-OTA-Application/kubernetes/deployments/server-deployment.yaml -n $NAMESPACE
-kubectl apply -f Secure-OTA-Application/kubernetes/deployments/app-deployment.yaml -n $NAMESPACE
-kubectl apply -f Secure-OTA-Application/kubernetes/deployments/ota-manager-deployment.yaml -n $NAMESPACE
-kubectl apply -f Secure-OTA-Application/kubernetes/services/ota-services.yaml -n $NAMESPACE
-
-# Deploy ECU-CORE components
-kubectl apply -f ECU-CORE/kubernetes/deployments/ros-master-deployment.yaml -n $NAMESPACE
-kubectl apply -f ECU-CORE/kubernetes/deployments/sensor-node-deployment.yaml -n $NAMESPACE
-kubectl apply -f ECU-CORE/kubernetes/deployments/control-node-deployment.yaml -n $NAMESPACE
-kubectl apply -f ECU-CORE/kubernetes/services/ros-services.yaml -n $NAMESPACE
-
-echo "SDV system deployment complete!"
